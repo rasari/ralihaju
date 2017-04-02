83bc9112c7c5a7d0b3def27b23f30764498298d8
Lisa <Lisa@LAPTOP-5JIE1K3A.localdomain>
2017-04-02 14:58:44 +0200

    Erstellen des Projekts

---
+++ src/htwg/in/gib/anam/GIB_AnamneseServlet.java
@@ -0,0 +1,13 @@
+package htwg.in.gib.anam;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class GIB_AnamneseServlet extends HttpServlet {
+	@Override
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
