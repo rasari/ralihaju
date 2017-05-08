9cda20d8c39e520bebce578209402ab66ae28f63
LisaWesthaeusser <lisawesthaeusser@gmail.com>
2017-05-08 18:27:21 +0200

    Anpassen von Pfaden für Startseite

--- src/htwg/in/gib/anam/GIB_AnamneseServlet.java
+++ src/htwg/in/gib/anam/GIB_AnamneseServlet.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 
+
 @SuppressWarnings("serial")
 public class GIB_AnamneseServlet extends HttpServlet {
 	
