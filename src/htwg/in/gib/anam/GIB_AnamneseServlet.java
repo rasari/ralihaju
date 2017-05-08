0e484dd0c412f8f3d25473a38aafd98c20acf2de
LisaWesthaeusser <lisawesthaeusser@gmail.com>
2017-05-08 15:31:18 +0200

    Funktion Servlet probieren

--- src/htwg/in/gib/anam/GIB_AnamneseServlet.java
+++ src/htwg/in/gib/anam/GIB_AnamneseServlet.java
@@ -1,12 +1,19 @@
 package htwg.in.gib.anam;
 
 import java.io.IOException;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
 public class GIB_AnamneseServlet extends HttpServlet {
+	
 	@Override
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+		req.getRequestDispatcher("../html/Anamnesebogen.html").include(req, resp); 
+		
+		logInName = req.getParameter("Klinik");
+		
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello ich teste die world");
 	}
