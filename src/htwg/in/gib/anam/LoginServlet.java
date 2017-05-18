892184b7f0ae6a32063fdd29b77ec8400f1dafdd
LisaWesthaeusser <lisawesthaeusser@gmail.com>
2017-05-18 16:23:11 +0200

    Revert "Anpassungen und Test, ob post-Methode funktioniert"
    This reverts commit b92aaa55e88ca92c2b38ade7b9f0e118d102bcf8.

--- src/htwg/in/gib/anam/LoginServlet.java
+++ src/htwg/in/gib/anam/LoginServlet.java
@@ -3,6 +3,7 @@ package htwg.in.gib.anam;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
