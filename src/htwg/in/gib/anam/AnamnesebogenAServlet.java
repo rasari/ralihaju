f340fcf04dee5b7081d888a6455d81c832057d96
rasari <rahime.sari@htwg-konstanz.de>
2017-05-20 09:48:32 +0200

    Login Anpassungen

--- src/htwg/in/gib/anam/AnamnesebogenAServlet.java
+++ src/htwg/in/gib/anam/AnamnesebogenAServlet.java
@@ -16,7 +16,7 @@ public class AnamnesebogenAServlet extends HttpServlet {
 
 		String vorname = req.getParameter("Vorname");
 		String nachname = req.getParameter("Nachname");
-		String antwortGeschlecht = req.getParameter("Antwort");
+		String antwortGeschlecht = req.getParameter("Geschlecht");
 		String gebDat = req.getParameter("gebDat");
 		String strasse = req.getParameter("Str");
 		String plz = req.getParameter("PLZ");
