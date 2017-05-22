0227a6e14d3f69e6b4540e68b8c43f7422b006d1
Jülide <Jlide@172.20.168.17>
2017-05-22 10:10:00 +0200

    Test Commit

--- src/htwg/in/gib/anam/PDF_Generierung.java
+++ src/htwg/in/gib/anam/PDF_Generierung.java
@@ -1,13 +1,6 @@
 package htwg.in.gib.anam;
 
-import java.awt.Desktop;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.OutputStream;
-
-import javax.servlet.http.HttpServletResponse;
 
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
